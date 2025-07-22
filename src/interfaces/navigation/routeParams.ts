1a3903095c766a5ae938aad0f4320ac9479feae2
Adriana <150181468+Adriana0301@users.noreply.github.com>
2025-07-22 17:13:55 +0300

    feat: added navigation, installed yup, formik, react-navigation, dotevn, created screens.

---
+++ src/interfaces/navigation/routeParams.ts
@@ -0,0 +1,19 @@
+import { ERouteNames } from './routeNames';
+
+export type AuthStackParamList = {
+  [ERouteNames.SIGN_IN_SCREEN]: undefined;
+  [ERouteNames.SIGN_UP_SCREEN]: undefined;
+};
+
+export type AppStackParamList = {
+  [ERouteNames.TABS_SCREEN]: undefined; // NavigatorScreenParams<TabsStackParamList>;
+  [ERouteNames.TASKS_SCREEN]: undefined; // {taskId: number}
+  [ERouteNames.CURRENT_USER]: undefined;
+  [ERouteNames.COMMON_TASKS_SCREEN]: undefined; // {taskId: number}
+};
+
+export type TabsStackParamList = {
+  [ERouteNames.TASKS_SCREEN]: undefined;
+  [ERouteNames.CURRENT_USER]: undefined;
+  [ERouteNames.COMMON_TASKS_SCREEN]: undefined;
+};
---
+++ src/screens/TaskEditorScreen/TaskEditorScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+const TaskEditorScreen = () => {
+  return (
+    <View>
+      <Text>TaskEditorScreen</Text>
+    </View>
+  );
+};
+export default TaskEditorScreen;
