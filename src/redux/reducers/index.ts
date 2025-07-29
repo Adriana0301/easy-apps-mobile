a5fd5132306b0aaa95352e573250c06ecfe9d484
feveron <146572725+feveron@users.noreply.github.com>
2025-07-29 15:43:51 +0300

    feature/EA-3 sign in screen logic (#6)
    * feature: added auth api, added types, added authSlice,
    
    * feat: auth flow improvements

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import authSlice from '../auth/authSlice';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+    auth : authSlice,
+});
 
 export default rootReducer;
