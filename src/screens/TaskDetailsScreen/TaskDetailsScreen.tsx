9272ffddce77e3c6f9c724bb8dc45abcc13df6ee
Adriana <150181468+Adriana0301@users.noreply.github.com>
2025-08-11 19:16:58 +0300

    feat: create current task in slice

--- src/screens/TaskDetailsScreen/TaskDetailsScreen.tsx
+++ src/screens/TaskDetailsScreen/TaskDetailsScreen.tsx
@@ -1,6 +1,32 @@
-import { Text } from 'react-native';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { useEffect } from 'react';
+import { Text, View } from 'react-native';
+import AppHeader from '../../components/AppHeader/AppHeader';
+import useTasks from '../../hooks/useTasks';
+import { ERouteNames } from '../../interfaces/navigation/routeNames';
+import { AppStackParamList } from '../../interfaces/navigation/routeParams';
+
+type TaskDetailsRouteProp = RouteProp<
+  AppStackParamList,
+  ERouteNames.TASK_DETAILS
+>;
 
 const TasksDetailsScreen = () => {
-  return <Text>TaskDetailsScreen</Text>;
+  const { currentTask, getTaskById } = useTasks();
+  const route = useRoute<TaskDetailsRouteProp>();
+  const { taskId } = route.params;
+  console.log(currentTask);
+
+  useEffect(() => {
+    getTaskById(taskId);
+  }, [taskId]);
+
+  return (
+    <View>
+      <AppHeader label="Task Details" goBackAllowed={true} />
+      <Text>{currentTask?.title}</Text>
+      <Text>{currentTask?.description}</Text>
+    </View>
+  );
 };
 export default TasksDetailsScreen;
