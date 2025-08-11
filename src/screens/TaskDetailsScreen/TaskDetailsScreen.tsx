0f8c0cec5b217bbc6aff6be5f27c85f315495e19
Adriana <150181468+Adriana0301@users.noreply.github.com>
2025-08-11 22:58:21 +0300

    feat: create markup and business logic for the Task Details screen

--- src/screens/TaskDetailsScreen/TaskDetailsScreen.tsx
+++ src/screens/TaskDetailsScreen/TaskDetailsScreen.tsx
@@ -1,10 +1,20 @@
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Alert, FlatList, SafeAreaView, Text, View } from 'react-native';
+import { useDispatch } from 'react-redux';
+import AppButton from '../../components/AppButton/AppButton';
 import AppHeader from '../../components/AppHeader/AppHeader';
+import DetailsTaskAttachmentsItem from '../../components/DetailsTaskAttachmentsItem/DetailsTaskAttachmentsItem';
+import ProgressStatus from '../../components/ProgressStatus/ProgressStatus';
 import useTasks from '../../hooks/useTasks';
 import { ERouteNames } from '../../interfaces/navigation/routeNames';
-import { AppStackParamList } from '../../interfaces/navigation/routeParams';
+import {
+  AppNavigationParams,
+  AppStackParamList,
+} from '../../interfaces/navigation/routeParams';
+import { deleteTaskAsyncAction } from '../../redux/actions/tasksActions';
+import { TAppDispatch } from '../../redux/store';
+import styles from './TaskDetailsScreen.styles';
 
 type TaskDetailsRouteProp = RouteProp<
   AppStackParamList,
@@ -12,21 +22,75 @@ type TaskDetailsRouteProp = RouteProp<
 >;
 
 const TasksDetailsScreen = () => {
-  const { currentTask, getTaskById } = useTasks();
+  const { currentTask, getTaskById, loading } = useTasks();
   const route = useRoute<TaskDetailsRouteProp>();
+  const dispatch = useDispatch<TAppDispatch>();
+  const navigation = useNavigation<AppNavigationParams>();
   const { taskId } = route.params;
-  console.log(currentTask);
+  const filesArray = currentTask?.files
+    ? JSON.parse(currentTask.files.toString())
+    : [];
 
   useEffect(() => {
     getTaskById(taskId);
   }, [taskId]);
 
+  const deleteTaskAndGoBack = () => {
+    dispatch(deleteTaskAsyncAction(taskId));
+    if (navigation.canGoBack()) {
+      navigation.goBack();
+    }
+  };
+  const deleteTaskButton = () =>
+    Alert.alert('Are you sure?', '', [
+      { text: 'Cancel', style: 'cancel' },
+      {
+        text: 'Yes',
+        onPress: () => deleteTaskAndGoBack(),
+      },
+    ]);
+
   return (
-    <View>
+    <SafeAreaView style={styles.rootContainer}>
       <AppHeader label="Task Details" goBackAllowed={true} />
-      <Text>{currentTask?.title}</Text>
-      <Text>{currentTask?.description}</Text>
-    </View>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.title}>{currentTask?.title}</Text>
+          <Text style={styles.description}>{currentTask?.description}</Text>
+          <ProgressStatus done={currentTask?.done} />
+          <FlatList
+            data={filesArray}
+            keyExtractor={(item, index) => index.toString()}
+            horizontal
+            renderItem={({ item }) => (
+              <DetailsTaskAttachmentsItem item={item} />
+            )}
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
+        <View style={styles.wrapperButtons}>
+          <View style={styles.wrapperButton}>
+            <AppButton
+              isLoading={loading}
+              title={'Delete'}
+              style={styles.button}
+              onPress={() => {
+                deleteTaskButton();
+              }}
+            />
+          </View>
+          <View style={styles.wrapperButton}>
+            <AppButton
+              title={'Edit'}
+              style={styles.button}
+              onPress={() =>
+                navigation.navigate(ERouteNames.TASK_EDITOR, { taskId: taskId })
+              }
+            />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 export default TasksDetailsScreen;
